4b44af2826ca2c5735a7d2c3f73a6513d34d0a55
PavelPiasetski <pyasecky2010pavel@mail.ru>
2021-08-05 16:27:42 +0300

    Login GUI added

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+CSRF_ENABLED = os.getenv('CSRF_ENABLED')
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
