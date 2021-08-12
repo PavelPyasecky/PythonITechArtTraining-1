696ece55717efe22c8b35358e0c874f155e8bddb
Pavel Piasetcki <pyasecky2010pavel@mail.ru>
2021-08-12 12:05:28 +0300

    Redirect to login page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+from flask import request
 from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_required
+from flask_login import login_user, logout_user
+from werkzeug.urls import url_parse
 from app.models import User
 from app import app
 from .forms import LoginForm
@@ -7,6 +10,7 @@ from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'nickname': 'Miguel'}
     posts = [
@@ -36,7 +40,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect('/index')
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html',
                            title='Sign In',
                            form=form,
