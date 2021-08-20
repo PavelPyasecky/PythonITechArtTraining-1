02b95519c727f55c7838a78c352cba7e56f668e6
Pavel Piasetcki <pyasecky2010pavel@mail.ru>
2021-08-20 17:54:30 +0300

    Adding pagination UX to template

--- app/routes.py
+++ app/routes.py
@@ -31,8 +31,41 @@ def index():
     posts = current_user.followed_posts().paginate(
         page, app.config["POSTS_PER_PAGE"], False
     )
+    next_url = (
+        url_for("index", page=posts.next_num) if posts.has_next else None
+    )
+    prev_url = (
+        url_for("index", page=posts.prev_num) if posts.has_prev else None
+    )
     return render_template(
-        "index.html", title="Home page", form=form, posts=posts.items
+        "index.html",
+        title="Home",
+        form=form,
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
+
+
+@app.route("/explore")
+@login_required
+def explore():
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config["POSTS_PER_PAGE"], False
+    )
+    next_url = (
+        url_for("explore", page=posts.next_num) if posts.has_next else None
+    )
+    prev_url = (
+        url_for("explore", page=posts.prev_num) if posts.has_prev else None
+    )
+    return render_template(
+        "index.html",
+        title="Explore",
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
     )
 
 
@@ -138,13 +171,3 @@ def unfollow(username):
     db.session.commit()
     flash(f"You are not following {username}.")
     return redirect(url_for("user", username=username))
-
-
-@app.route("/explore")
-@login_required
-def explore():
-    page = request.args.get("page", 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page, app.config["POSTS_PER_PAGE"], False
-    )
-    return render_template("index.html", title="Explore", posts=posts.items)
