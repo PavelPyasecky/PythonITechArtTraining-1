1a2a7a6742d6c271d1bc14ad821ccdd95c954b70
PavelPiasetski <pyasecky2010pavel@mail.ru>
2021-08-27 16:27:02 +0300

    Mail instance added & email.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_recaptcha import ReCaptcha
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 from config import Config
 
@@ -13,6 +14,8 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+mail = Mail(app)
 
 login = LoginManager(app)
 login.login_view = "login"
